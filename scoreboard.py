c2a4f1c7afda9996f73c95c9a876f6508befeb22
Austen Rhyce Erickson <a.r.erickson.28@gmail.com>
2021-02-21 15:08:18 -0600

    added scoreboard

---
+++ scoreboard.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle) :
+    def __init__(self) :
+        super().__init__()
+        self.hideturtle()
+        self.score = 0
+        self.color("blue")
+        self.write(f"Score: {self.score}", move = False, align = "center", font = ("Papyrus", 20, "normal"))
+        
+        
+
+    def update_score(self) :
+        self.score += 1
+        self.clear()
+        self.write(f"Score: {self.score}", move = False, align = "center", font = ("Papyrus", 20, "normal"))
+    
--- snake_game.py
+++ snake_game.py
@@ -11,6 +11,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -21,6 +22,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -38,6 +40,7 @@ while game_is_on :
     #detect collison
     if snake.head.distance(food) < 15 :
         food.refresh()
+        scoreboard.update_score()
 
 
 
